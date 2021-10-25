1b92c653559cebac220b4da2d5481f2670ed6c37
Dmstanto <71608009+Dmstanto@users.noreply.github.com>
2021-10-24 22:10:10 -0400

    Create DDG.py

---
+++ DDG.py
@@ -0,0 +1,12 @@
+import requests
+
+url_ddg = "https://api.duckduckgo.com"
+
+response = request.get(url_ddg)
+
+president_list = [Adams,Arthur,Biden,Buchanan,Bush,Carter,Cleveland,Clinton,Coolidge,Eisenhower,Fillmore,Ford,Garfield,Grant,Harding,
+     Harrison,Hayes,Hoover,Jackson,Jefferson,Johnson,Kennedy,Lincoln,Madison, McKinley,Monroe,Nixon,Obama,Reagan,
+     Roosevelt,Taft,Taylor,Truman,Trump,Tyler,Van,Washington,Wilson]
+
+for president_list in url_ddg
+  return true
